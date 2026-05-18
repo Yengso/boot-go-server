68783563143902ec6c68c3e3240b03efd5955306
Sondre Engstrøm <sondre.engstrom@hotmail.com>
2026-05-18 21:43:36 +0200

    begging of http server

--- main.go
+++ main.go
@@ -3,25 +3,66 @@ package main
 import (
 	"log"
 	"net/http"
-	"time"
+	"sync/atomic"
+	"fmt"
 )
 
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
+	hits := fmt.Sprintf(`
+	<html>
+  		<body>
+    		<h1>Welcome, Chirpy Admin</h1>
+    		<p>Chirpy has been visited %d times!</p>
+  		</body>
+	</html>`, cfg.fileserverHits.Load())
+	
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(hits))
+}
+
+func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits.Store(0)
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+}
 
 
 func main() {
 	mux := http.NewServeMux()
 
+	apiCfg := apiConfig{}
+
 	s := &http.Server{
-		Addr:			":8000",
+		Addr:			":8080",
 		Handler:		mux,
-		ReadTimeout:	10 * time.Second,
-		WriteTimeout:	10 * time.Second,
 		MaxHeaderBytes: 1 << 20,
 	}
 	
-	mux.Handle("/", http.FileServer(http.Dir(".")))
-	mux.Handle("/assets", http.FileServer(http.Dir(".")))
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(".")))))
 
+	mux.HandleFunc("GET /api/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+
+	})
+
+	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
+	mux.HandleFunc("POST /admin/reset", apiCfg.handlerReset)
 
 
 	log.Fatal(s.ListenAndServe())
