68b6890059445cd5da7a6ade62ed995a5cbfd700
Sondre Engstrøm <sondre.engstrom@hotmail.com>
2026-05-28 21:28:55 +0200

    setup postgresql

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"net/http"
 	"sync/atomic"
 	"fmt"
+	"encoding/json"
+	"strings"
 )
 
 
@@ -40,6 +42,29 @@ func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 }
 
+func respondWithError(w http.ResponseWriter, code int, msg string) {
+	type returnVals struct {
+		Error string `json:"error"`
+	}
+
+	respBody := returnVals{
+		Error: msg,
+	}
+	respondWithJSON(w, code, respBody)
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	data, err := json.Marshal(payload)
+	if err != nil {
+		log.Printf("Error marshalling JSON: %s", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(data)
+}
+
 
 func main() {
 	mux := http.NewServeMux()
@@ -61,9 +86,48 @@ func main() {
 
 	})
 
+	mux.HandleFunc("POST /api/validate_chirp", func(w http.ResponseWriter, r *http.Request){
+		type parameters struct {
+			Body string `json:"body"`
+		}
+
+		decoder := json.NewDecoder(r.Body)
+		params := parameters{}
+		err := decoder.Decode(&params)
+		if err != nil {
+			respondWithError(w, 400, "Something went wrong")
+			return
+		}
+		if len(params.Body) > 140 {
+			respondWithError(w, 400, "Chirp is too long")
+			return
+		}
+
+		badWords := map[string]struct{}{
+			"kerfuffle": {},
+			"sharbert": {},
+			"fornax": {},
+		}
+		
+		sentence := strings.Split(params.Body, " ")
+		for i, word := range sentence {
+			if _, ok := badWords[strings.ToLower(word)]; ok {
+				sentence[i] = "****"
+			}
+		}
+		cleanedSentence := strings.Join(sentence, " ")
+
+		type returnVals struct {
+			CleanedBody string `json:"cleaned_body"`
+		}
+		respBody := returnVals{
+			CleanedBody: cleanedSentence,
+		}
+		respondWithJSON(w, http.StatusOK, respBody)
+	})
+
 	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
 	mux.HandleFunc("POST /admin/reset", apiCfg.handlerReset)
-
 
 	log.Fatal(s.ListenAndServe())
 }
